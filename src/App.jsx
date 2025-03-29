c22bc59ea90f1f057da0ea13a2aae32397484ae6
chkg2a <xshalltearvamps@gmail.com>
2025-03-29 09:23:12 +0530

    2025-03-29 09:23:12

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
