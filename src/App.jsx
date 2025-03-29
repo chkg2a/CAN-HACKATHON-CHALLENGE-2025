5d4ac4fa7a3d38dcc0071ba57fee7258bd9fecf2
pkbtc <parasarkashyap06@gmail.com>
2025-03-29 10:23:28 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.css";
-
+import ResumeExtractor from "./components/ResumeExtractor";
 function App() {
   return (
     <div className="h-screen flex justify-center w-screen">
@@ -9,6 +9,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/resume-extractor" element={<ResumeExtractor />} />
           </Routes>
         </BrowserRouter>
       </div>
