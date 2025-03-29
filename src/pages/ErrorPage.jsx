1bcb8d55504d550976b78f9725bb7ea796237ca8
tolen-kangleicha <tolenkangleicha@gmail.com>
2025-03-29 10:33:10 +0530

    Added 404 Error Page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -4,18 +4,6 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-black text-white">
-      //<header className="absolute top-5 left-5 flex justify-between w-full px-10 text-gray-400 text-sm">
-      //  <span>ALEX TKACHEV</span>
-      //  <span className="text-green-400">• AVAILABLE FOR FREELANCE</span>
-      //  <div className="space-x-4">
-      //    <a href="#" className="hover:text-gray-200">DRIBBBLE</a>
-      //    <a href="#" className="hover:text-gray-200">INSTAGRAM</a>
-      //    <button className="bg-lime-400 text-black px-4 py-1 rounded hover:bg-lime-500">
-      //      LET'S TALK
-      //    </button>
-      //  </div>
-      //</header>
-
       {/* Main Content */}
       <main className="text-center">
         <h1 className="text-8xl font-bold">
