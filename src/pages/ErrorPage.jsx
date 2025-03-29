a4f5a77d8480c3b89ad6458a4d32ae33272cd944
tolen-kangleicha <tolenkangleicha@gmail.com>
2025-03-29 10:12:10 +0530

    Added 404 Error Page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar"; // Import the Navbar component
 
 const ErrorPage = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-black text-white">
-      {/* Header */}
-      <header className="absolute top-5 left-5 flex justify-between w-full px-10 text-gray-400 text-sm">
-        <span>ALEX TKACHEV</span>
-        <span className="text-green-400">• AVAILABLE FOR FREELANCE</span>
-        <div className="space-x-4">
-          <a href="#" className="hover:text-gray-200">DRIBBBLE</a>
-          <a href="#" className="hover:text-gray-200">INSTAGRAM</a>
-          <button className="bg-lime-400 text-black px-4 py-1 rounded hover:bg-lime-500">
-            LET'S TALK
-          </button>
-        </div>
-      </header>
+      {/* Navbar */}
+      <Navbar />
 
       {/* Main Content */}
-      <main className="text-center">
+      <main className="text-center mt-10">
         <h1 className="text-8xl font-bold">
           4<span className="text-lime-400">👁</span>4
         </h1>
